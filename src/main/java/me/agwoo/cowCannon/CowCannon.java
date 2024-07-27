5eb5239fbc76a9acb435cea09d7bc880d0ade686
agwoo <agwoowee@outlook.com>
2024-07-27 12:17:09 +0200

    Initial commit

---
+++ src/main/java/me/agwoo/cowCannon/CowCannon.java
@@ -0,0 +1,17 @@
+package me.agwoo.cowCannon;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CowCannon extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
